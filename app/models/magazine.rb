9b8e528c21bfa3bbb34e8ea5458bef90c1ff0f7b
Chau Tran <cctran96@gmail.com>
2021-05-26 10:55:09 -0700

    Done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,12 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list
+    readers.map{|reader| reader.email}.join(";")
+  end
+
+  def self.most_popular
+    Subscription.all.group(:magazine_id).order('COUNT(magazine_id) DESC').first.magazine
+  end
 end
